7ed5a2fd91cafad29cdec939e26d75c73a208f7d
Silica163 <96188282+Silica163@users.noreply.github.com>
2022-01-15 20:43:01 +0700

    update open popup

--- openpopup/script.js
+++ openpopup/script.js
@@ -1,3 +1,8 @@
 var openp = (link,height,width) =>{
     open(`${link}`,'',`width:${width}px,height:${height}px`);
 }
+var getvarlue = ()=>{
+    var link = document.getElementsByName("link")[0];
+    var wid = document.getElementsByName("wid")[0];
+    var hei = document.getElementsByName("hei")[0];
+}
