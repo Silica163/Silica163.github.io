03a5e3825e13dc57053dcff88df8bb0b0644e565
Silica163 <96188282+Silica163@users.noreply.github.com>
2022-01-15 21:11:15 +0700

    .

--- openpopup/script.js
+++ openpopup/script.js
@@ -1,10 +1,10 @@
 var openp = (link,height,width) =>{
     open(`${link}`,'',`width:${width}px,height:${height}px`);
 }
-var getvarlue = (e)=>{
+var getvarlue = ()=>{
     var link = document.getElementsByName("link")[0];
     var wid = document.getElementsByName("wid")[0];
     var hei = document.getElementsByName("hei")[0];
     console.log(this);
 }
-document.addEventListener('keyup',getvarlue);
+document.addEventListener('keyup',getvarlue());
